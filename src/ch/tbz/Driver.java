56135a70f05539adc48517a77a822e47b3298b8b
denis99 <d.rykart@hispeed.ch>
2018-01-23 10:21:25 +0100

    Reader

--- src/ch/tbz/Driver.java
+++ src/ch/tbz/Driver.java
@@ -9,6 +9,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import static java.util.logging.Level.parse;
+
 
 public class Driver {
 
@@ -21,25 +23,26 @@ public class Driver {
             URL oracle = new URL("http://api.brewerydb.com/v2/styles?key=1511d0db4a1d6841481c672455358cff"); // URL to Parse
             URLConnection yc = oracle.openConnection();
             BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            JSONParser jsonParser = new JSONParser();
 
             String inputLine;
             while ((inputLine = in.readLine()) != null) {
-                JSONParser jsonParser = new JSONParser();
-                Object obj  = jsonParser.parse(inputLine);
-                JSONArray array = new JSONArray();
-                array.add(obj);
-                // Loop through each item
-                for (Object o : array) {
-                    JSONObject tutorials = (JSONObject) o;
+                Object obj = jsonParser.parse(inputLine);
+                JSONObject tutorials = (JSONObject) obj;
 
-                    Long id = (Long) tutorials.get("id");
+                JSONArray data = (JSONArray) tutorials.get("data");
+
+                for (Object style : data) {
+                    JSONObject styleJSON = (JSONObject) style;
+
+                    long id = (long) styleJSON.get("id");
                     System.out.println("ID : " + id);
 
-                    String name = (String) tutorials.get("name");
+                    String name = (String) styleJSON.get("name");
                     System.out.println("Name: " + name);
-
-                    System.out.println("\n");
                 }
+
+                System.out.println("\n");
             }
             in.close();
         } catch (FileNotFoundException e) {
